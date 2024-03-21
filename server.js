39a85d1aeb4db2836938fdcddbe8f1d6f971a57c
iamsurajx <surajc8800@gmail.com>
2024-03-21 22:26:39 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,59 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  let filePath = '.' + req.url;
+
+  if (filePath === './') {
+    filePath = './index.html'; // Serve index.html by default
+  }
+
+  const extname = path.extname(filePath);
+  let contentType = 'text/html';
+
+  // Determine Content-Type based on file extension
+  switch (extname) {
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.json':
+      contentType = 'application/json';
+      break;
+    case '.png':
+      contentType = 'image/png';
+      break;
+    case '.jpg':
+      contentType = 'image/jpg';
+      break;
+    case '.ico':
+      contentType = 'image/x-icon';
+      break;
+  }
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        // File not found
+        res.writeHead(404);
+        res.end('404 Not Found');
+      } else {
+        // Server error
+        res.writeHead(500);
+        res.end('500 Internal Server Error: ' + err.code);
+      }
+    } else {
+      // Success, send the file
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf-8');
+    }
+  });
+});
+
+const PORT = process.env.PORT || 3000; // Use port 3000 by default
+server.listen(PORT, () => {
+  console.log(`Server running on port http://localhost:${PORT}`);
+});
